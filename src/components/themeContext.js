c10faaff97f3990b97814056b9bbe3d3dd71918d
naveendk <naveenkolambage@gmail.com>
2020-06-27 17:59:38 +1200

    b4-converting-themeContext-functional

--- src/components/themeContext.js
+++ src/components/themeContext.js
@@ -2,12 +2,10 @@ import React, {Component} from "react"
 const ThemeContext = React.createContext()
 
 /**
- * Challenge:
- * 1) Add state to hold the current theme
- * 2) Add a method for flipping the state between light and dark
- * 
+ *Convert this ThemeContextProvider into a functional component that uses hooks
+ * Tip: Use the browser dev tools if you run into a bug that isn't clear from the Scrimba console logs
  */
-
+ 
 class ThemeContextProvider extends Component {
     state = {
         theme: "dark"
