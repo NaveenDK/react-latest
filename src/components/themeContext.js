d8bfe06c772bfdeabaea03131de25435ce2c9ea1
naveendk <naveenkolambage@gmail.com>
2020-06-27 19:00:44 +1200

    converted themecontext.js to functional

--- src/components/themeContext.js
+++ src/components/themeContext.js
@@ -1,31 +1,26 @@
-import React, {Component} from "react"
+import React, {useState} from "react"
 const ThemeContext = React.createContext()
 
-/**
- *Convert this ThemeContextProvider into a functional component that uses hooks
- * Tip: Use the browser dev tools if you run into a bug that isn't clear from the Scrimba console logs
- */
- 
-class ThemeContextProvider extends Component {
-    state = {
-        theme: "dark"
+function ThemeContextProvider(props){
+    const [ theme,setTheme] = useState("dark")
+     
+    function toggleTheme(){
+        setTheme(prevTheme => prevTheme === "light" ? "dark" : "light")
+        console.log(theme);
+        
     }
-    
-    toggleTheme = () => {
-        this.setState(prevState => {
-            return {
-                theme: prevState.theme === "light" ? "dark" : "light"
-            }
-        })
-    }
-    
-    render() {
-        return (
-            <ThemeContext.Provider value={{theme: this.state.theme, toggleTheme: this.toggleTheme}}>
-                {this.props.children}
-            </ThemeContext.Provider>
-        )
-    }
+
+    return(
+        <div>
+               <ThemeContext.Provider 
+               value={{theme, toggleTheme}}
+               >
+             {props.children}           
+             </ThemeContext.Provider>
+
+        </div>
+    )
+
 }
 
 export {ThemeContextProvider, ThemeContext}
