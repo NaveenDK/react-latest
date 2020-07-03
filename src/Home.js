391b812ff864441d61666d572fabc954da436686
naveendk <naveenkolambage@gmail.com>
2020-07-04 09:38:12 +1200

    added few items for this

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,6 @@
 import React from "react"
-import {useLocation, useParams, useHistory, useRouteMatch} from "react-router-dom"
 
-function Home(props) {
+function Home() {
     return (
         <h1>Home Page</h1>
     )
