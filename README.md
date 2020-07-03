dd6bc7f5020aacab5be5bf17a0a848f716d41e15
naveendk <naveenkolambage@gmail.com>
2020-07-03 23:32:20 +1200

    added readme text

--- README.md
+++ README.md
@@ -17,3 +17,4 @@ Convert existing render props to customHooks -   `b4-turning-toggler-customhooks
 Using custom hooks with the speed type game -  `b4-speedtyping-game-with-hooks`
 Backup of everything before react-router-props - `b4-react-router-props`
 Please start the react router nested routes here - `b-reat-router-nested`
+Branch for practicing react-router-params - `b4-react-router-params`
--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from "react"
 import Header from "./components/Header"
-import Footer from "./components/Footer"
 import Home from "./pages/Home"
-import Profile from "./pages/profile/Profile"
-import Info from "./pages/profile/Info"
-import Settings from "./pages/profile/Settings"
+import ServicesList from "./pages/services/ServicesList"
+import ServiceDetail from "./pages/services/ServiceDetail"
 
 import {Switch, Route} from "react-router-dom"
 
@@ -17,18 +15,10 @@ function App() {
                 <Route exact path="/">
                     <Home />
                 </Route>
-                <Route exact path="/profile">
-                    <Profile />
-                </Route>
-                <Route path="/profile/info">
-                    <Info/>
-                </Route>
-                <Route path="/profile/settings">
-                    <Settings/>
+                <Route exact path="/services" >
+                    <ServicesList />
                 </Route>
             </Switch>
-            
-            <Footer />
         </div>
     )
 }
